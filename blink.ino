da4c7dae98ac30a4ac8f8c52981a8d9faa4569ab
YahuiSun <syhhit@gmail.com>
2023-03-04 14:24:31 +0800

    Update blink.ino

--- blink.ino
+++ blink.ino
@@ -3,7 +3,7 @@
   Turns on an LED on for one second, then off for one second, repeatedly.
  
 请通过Pull request功能在下面写下自己的学号和姓名（每行一人）：
-20210129 孙亚辉
+3566
  */
  
 // Pin 13 has an LED connected on most Arduino boards.
